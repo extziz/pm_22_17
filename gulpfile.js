6f70b4f0884526a5a0aaba746f224b94b69e9058
extziz <malahovakata638@gmail.com>
2025-09-23 20:55:12 +0300

    feat: fixed watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-import { src, dest, watch, series } from "gulp";
+import { src, dest, watch, series, parallel } from "gulp";
 import gulpSass from "gulp-sass"
 import cssnano from "gulp-cssnano";
 import * as saasModule from "sass"
 import sourcemaps from "gulp-sourcemaps";
 import browserSyncModule from 'browser-sync'
 import fileInclude from "gulp-file-include";
+import rename from "gulp-rename";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import imagemin from "gulp-imagemin";
+
 
 const sass = gulpSass(saasModule);
 const browserSync = browserSyncModule.create();
@@ -24,8 +29,7 @@ function html() {
 function styles() {
     return src("app/scss/**/*.scss")   
         .pipe(sourcemaps.init())         
-        .pipe(sass().on("error", sass.logError)) 
-        .pipe(dest("dist/css"))         
+        .pipe(sass().on("error", sass.logError))       
         .pipe(cssnano())                 
         .pipe(rename({ suffix: ".min" })) 
         .pipe(sourcemaps.write("."))     
@@ -46,4 +50,37 @@ function images(){
           .pipe(imagemin())
           .pipe(dest('dist/images'));
 }
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    },
+    notify: false
+  });
+}
+
+function reload() {
+  browserSync.reload();
+  
+}
+
+function watcher(){
+  serve();
+  watch("./app/scss/**/*.scss", {events: "change"}, reload)
+  watch("./app/*.html", {events: "change"}, reload).addListener("change", reload)
+  watch("./app/js/**/*.js", {events: "change"}, reload).addListener("change", reload)
+//   watch("./app/images/**/*", series(images, reload));
+}
+
+
+const seriesModule = series(
+  html,
+  styles,
+  scripts,
+  images,
+  watcher
+);
+export default seriesModule;
+
 
